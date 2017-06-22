adc4085c7587e764cf8390b6bab4e0995e59cf16
Alex Vaccari <alexvaccari@Alexs-MacBook-Air.local>
2017-06-21 23:01:11 -0400

    Added some more code

--- src/WW.java
+++ src/WW.java
@@ -2,23 +2,38 @@
  * Created by alexvaccari on 6/21/17.
  */
 
+/*
+* Simple SIR model to find the number of generations a virus survives until a population is immune or infected.
+ */
 import java.util.*;
 import java.math.*;
+import java.io.*;
+import java.lang.*;
 
 public class WW {
     final int initalPopulation = 10000;
 
     public static void main(String[] args){
-        int infected;
-        double infectionCoefficient;
-        double immunityCoefficient;
-        double populationImmune;
+        double infectedC;
+        double immuneC;
+        double survivedC;
 
-        Scanner userInput = new Scanner(System.in);
-        
+        Console c = System.console();
+        String y = c.readLine();
 
+        String[] parts = y.split(" ");
+
+        if(parts.length != 4){
+            System.out.print("Error: 4 args required");
+            System.exit(0);
+        }
+
+        int infected = findInfected(initialPopulation, infectedC);
+
+        int survived = findSurvived();
+
+        int immune  = findImmune();
     }
 
-
-
+    public static int findInfected
 }
