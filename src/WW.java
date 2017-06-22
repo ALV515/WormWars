37aea9e1fb832653eaacc2331fb97e5ef0e6fc5f
Alex Vaccari <alexvaccari@Alexs-MacBook-Air.local>
2017-06-21 22:28:33 -0400

    First Commit of Worm Wars

---
+++ src/WW.java
@@ -0,0 +1,24 @@
+/**
+ * Created by alexvaccari on 6/21/17.
+ */
+
+import java.util.*;
+import java.math.*;
+
+public class WW {
+    final int initalPopulation = 10000;
+
+    public static void main(String[] args){
+        int infected;
+        double infectionCoefficient;
+        double immunityCoefficient;
+        double populationImmune;
+
+        Scanner userInput = new Scanner(System.in);
+        
+
+    }
+
+
+
+}
